03e3f32ee22c897bfd52c3bff44c575c26e83acb
gabrieltailand <gabrieltailand@gmail.com>
2021-11-17 17:51:41 +0000

    Add OSB of descreption

--- mundo02/ex69b.py
+++ mundo02/ex69b.py
@@ -2,7 +2,7 @@
 se o usuário quer ou não continuar. No final, mostre:
 A) quantas pessoas tem mais de 18 anos.
 B) quantos homens foram cadastrados.
-C) quantas mulheres tem menos de 20 anos.'''
+C) quantas mulheres tem menos de 20 anos. OBS'''
 import datetime
 from datetime import date
 total18 = 0
